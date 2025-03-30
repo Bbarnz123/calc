9ba37ae5037259797d29c08767a0afb434c1a2f8
Bbarnz123 <barneyberesford@gmail.com>
2025-03-30 20:25:10 +0100

    create calc display and buttons, write operation functions

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # calc
+
+The final boss of The Odin Project: Foundations. The calculator.
+
+Let's hope and pray we can get it done!
+
+Peace.
---
+++ script.js
@@ -0,0 +1,41 @@
+function add (a, b) {
+    return a + b;
+}
+
+function subtract (a, b) {
+    return a - b;
+}
+
+function multiply (a, b) {
+    return a * b;
+}
+
+function divide (a, b) {
+    return a / b;
+}
+
+function operate (operator, a, b) {
+
+    switch (operator) {
+        case "+":
+            return add (a, b);
+        case "-":
+            return subtract (a, b);
+        case "*":
+            return multiply (a, b);
+        case "/":
+            return divide (a, b);
+    }
+
+}
+
+let a;
+let b;
+let operator;
+
+const buttons = document.querySelectorAll("button");
+const buttonsArray = [...buttons];
+
+buttonsArray.forEach((item) => {
+    item.addEventListener("click", )
+})
