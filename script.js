7a8b73a82adf7f0c9f65f91f222c4e598d5348fb
Bbarnz123 <barneyberesford@gmail.com>
2025-03-31 22:42:43 +0100

    create operate function, update screen each time button pressed, run operate after equal sign pressed

--- script.js
+++ script.js
@@ -15,7 +15,6 @@ function divide (a, b) {
 }
 
 function operate (operator, a, b) {
-
     switch (operator) {
         case "+":
             return add (a, b);
@@ -26,16 +25,64 @@ function operate (operator, a, b) {
         case "/":
             return divide (a, b);
     }
+}
+
+function addToDisplay (event) {
+    if (result !== undefined) {
+        wipeScreen ();
+        result = undefined;
+    }
+    screen.textContent += event.target.textContent;
+}
+
+function wipeScreen () {
+    screen.textContent = "";
 
 }
 
 let a;
 let b;
 let operator;
+let result;
 
-const buttons = document.querySelectorAll("button");
-const buttonsArray = [...buttons];
+const digits = document.querySelectorAll(".digits");
+const digitsArray = [...digits];
+digitsArray.forEach((item) => {
+    item.addEventListener("click", addToDisplay)
+});
 
-buttonsArray.forEach((item) => {
-    item.addEventListener("click", )
-})
+const screen = document.querySelector("#screen");
+
+const equal = document.querySelector("#equal");
+equal.addEventListener("click", (event) => {
+    if (operator === undefined || result !== undefined) {
+        return;
+    }
+
+    b = parseInt(screen.textContent);
+    wipeScreen ();
+    result = operate (operator, a, b);
+
+    if (result.toString().length > 5) {
+        result = result.toString().split("").slice(0, 6).join("");
+    };
+
+    screen.textContent = result;
+});
+
+const operators = document.querySelectorAll(".operators");
+const operatorsArray = [...operators];
+operatorsArray.forEach((item) => {
+    item.addEventListener("click", (event) => {
+        if (screen.textContent !== "") {
+            a = parseInt(screen.textContent);
+        };
+        wipeScreen ();
+        operator = event.target.textContent;
+    });
+});
+
+clear = document.querySelector("#clear");
+clear.addEventListener("click", () => {
+    wipeScreen ();
+});
