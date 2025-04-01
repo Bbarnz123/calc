44e7c5e6ebf48fc4fa2eb147ae891edcbc19a6fe
Bbarnz123 <barneyberesford@gmail.com>
2025-04-01 14:10:50 +0100

    add keyboard support, decimal support, more than two calculations in a row

--- script.js
+++ script.js
@@ -40,6 +40,22 @@ function wipeScreen () {
 
 }
 
+function calculateAndShow () {
+    if (operator === undefined || result !== undefined) {
+        return;
+    }
+
+    b = parseInt(screen.textContent);
+    wipeScreen ();
+    result = operate (operator, a, b);
+
+    if (result.toString().length > 10) {
+        result = result.toString().split("").slice(0, 6).join("");
+    };
+
+    screen.textContent = result;
+}
+
 let a;
 let b;
 let operator;
@@ -54,29 +70,20 @@ digitsArray.forEach((item) => {
 const screen = document.querySelector("#screen");
 
 const equal = document.querySelector("#equal");
-equal.addEventListener("click", (event) => {
-    if (operator === undefined || result !== undefined) {
-        return;
-    }
-
-    b = parseInt(screen.textContent);
-    wipeScreen ();
-    result = operate (operator, a, b);
-
-    if (result.toString().length > 10) {
-        result = result.toString().split("").slice(0, 6).join("");
-    };
-
-    screen.textContent = result;
-});
+equal.addEventListener("click", calculateAndShow);
 
 const operators = document.querySelectorAll(".operators");
 const operatorsArray = [...operators];
 operatorsArray.forEach((item) => {
     item.addEventListener("click", (event) => {
+        if (a !== undefined) {
+            calculateAndShow ();
+        }
+
         if (screen.textContent !== "") {
             a = parseInt(screen.textContent);
         };
+
         wipeScreen ();
         operator = event.target.textContent;
     });
@@ -85,4 +92,28 @@ operatorsArray.forEach((item) => {
 clear = document.querySelector("#clear");
 clear.addEventListener("click", () => {
     wipeScreen ();
+    a = undefined;
+    b = undefined;
+    operator = undefined;
+    result = undefined;
+});
+
+const point = document.querySelector("#point");
+point.addEventListener("click", () => {
+    if (screen.textContent.split("").includes(".")) {
+        return;
+    };
+    screen.textContent += ".";
+});
+
+const del = document.querySelector("#del");
+del.addEventListener("click", () => {
+    screen.textContent = screen.textContent.substring(0, screen.textContent.length - 1);
+});
+
+const body = document.querySelector("body");
+body.addEventListener("keypress", (event) => {
+    if (event.keyCode >= 48 && event.keyCode <= 57) {
+        screen.textContent += event.key;
+    };
 });
